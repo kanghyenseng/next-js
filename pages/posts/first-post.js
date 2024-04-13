5405a378311ae482c231332dcc2763fef8c297ac
kanghyenseng <kanghyenseng@naver.com>
2024-04-13 11:09:36 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hello! My name is Kang Hyeon-seong. I'm 22years old. I am a 24th grader majoring in information system engineering at Hanyang University. My hometown is Geoje. My mbti is Infp. My hobby is playing soccer, basektball and gmae, League of legends. </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
